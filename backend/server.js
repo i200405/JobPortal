01fd71e354883f8754930d078c9c79ddc8039630
Hamza <hamzasalarian@gmail.com>
2024-08-06 18:21:06 +0500

    Job Finding Platform

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,11 +13,11 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // MongoDB Connection
-mongoose.connect('mongodb://localhost:27017/jobBoardDB', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => console.log('MongoDB connected...'))
-.catch(err => console.log(err));
+  .catch(err => console.log('MongoDB connection error:', err));
 
 // Serve static files from the public folder
 app.use(express.static(path.join(__dirname, '../public')));
@@ -27,15 +28,15 @@ app.use('/api/jobs', require('./routes/jobs'));
 
 // Basic route to serve the main index.html
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // Catch-all route to handle AngularJS HTML5 mode
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../public', 'index.html'));
 });
 
 // Start server
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
